355f8836bf6ed31c238d7197f764adb37b479684
Cleiton Santos <cleitonsantosgba@gmail.com>
2022-10-16 21:37:41 -0300

    Initial commit

---
+++ estilos/introducao.js
@@ -0,0 +1,64 @@
+// Comando que exibe informações no console
+// console.log("Olá Mundo")
+
+// Variaveis e tipos de dados
+// String
+//let nome = "Creudi Leudy"
+//console.log(nome)
+
+// number
+//let idade = 24
+//console.log(idade)
+
+// boolean
+//let ehProgramador = true
+//console.log(ehProgramador)
+
+// Operações matemáticas
+//let num1 = 10
+//let num2 = 5
+
+// Soma
+//let resultado = num1 + num2
+//console.log(resultado)
+
+// Arrays 
+//let notas = [10, 8, 7, 9]
+//console.log(notas)
+//console.log(notas[3])
+
+//notas[0] = 8.5
+//console.log(notas)
+
+//notas.push(9.1)
+//console.log(notas)
+
+// Laços de repetição
+//let notas = [10, 8, 7, 8]
+
+// For tradicional
+//for (let i = 0; i < 4; i++) {
+    //console.log(notas[i])
+//}
+//console.log("Terminou o for")
+
+// For of
+//for (let nota of notas) {
+    //console.log(nota);
+//}
+//console.log("Terminou o for of-'")
+
+// Objetos JavaScript
+//let aluno = {
+//    nome: "Creudi Leudy",
+//    idade: 23,
+//    notas: [10, 8, 7, 9],
+ //   contato: {
+//        telefone: "(88) 4002-8922",
+//        email: "creudileudy@email.org"
+//    }
+//}
+//console.log(aluno)
+//console.log(aluno.nome)
+//console.log(aluno.notas[0])
+//console.log(aluno.contato.email)
